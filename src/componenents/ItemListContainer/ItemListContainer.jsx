cda42fb6332f2155febdf734b3dd2e1e5f6d5ce5
Mardonio Abreu <mardonio.abreu@protonmail.com>
2024-03-16 16:08:37 -0600

    segunda entrega

--- src/componenents/ItemListContainer/ItemListContainer.jsx
+++ src/componenents/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,15 @@
 
+import { useContext } from "react";
+import cartContext from "../../context/cartContext";
 export function ItemListContainer({text}) {
-  return <p>{text}</p>;
+
+  const greeting = useContext(cartContext);
+
+  return 
+  <>
+    <p>{text}</p>;
+    <p>greeting</p>
+  </>
 }
 
 export default ItemListContainer;
---
+++ src/context/cartContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react"
+
+const cartContext = createContext("default greeting");
+
+export default cartContext;
